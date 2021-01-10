7c1225374fe01744e8656d0ec6059e6ac299c91c
inokawa <48897392+inokawa@users.noreply.github.com>
2021-01-10 19:59:32 +0900

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export default () => {
-  // TODO
+// @ts-expect-error
+import parseDot from "dotparser";
+
+export const parse = (text: string) => {
+  const ast = parseDot(text);
+  if (ast.length > 1) {
+    throw new Error("Dot file contains multiple graphs.");
+  }
+  return parseDot(text);
 };
