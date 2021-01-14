eaccd6aa484881b2f44c53de9f69eea4ff7949cd
inokawa <48897392+inokawa@users.noreply.github.com>
2021-01-15 01:09:52 +0900

    Impl renders

--- examples/index.stories.tsx
+++ examples/index.stories.tsx
@@ -7,39 +7,52 @@ export default {
 
 export const Letters = () => {
   return (
-    <Dot
-      data={
-        parse(`
-  digraph G {
-  
-      subgraph cluster_0 {
-          style=filled;
-          color=lightgrey;
-          node [style=filled,color=white];
-          a0 -> a1 -> a2 -> a3;
-          label = "process #1";
-      }
-  
-      subgraph cluster_1 {
-          node [style=filled];
-          b0 -> b1 -> b2 -> b3;
-          label = "process #2";
-          color=blue
-      }
-      start -> a0;
-      start -> b0;
-      a1 -> b3;
-      b2 -> a3;
-      a3 -> a0;
-      a3 -> end;
-      b3 -> end;
-  
-      start [shape=Mdiamond];
-      end [shape=Msquare];
-  }
-  
+    <svg width="1000" height="1000">
+      <g transform="translate(50,50)">
+        <Dot
+          data={
+            parse(`
+  // https://graphviz.org/Gallery/directed/fsm.html
+
+digraph finite_state_machine {
+	rankdir=LR;
+	size="8,5"
+	node [shape = doublecircle]; LR_0 LR_3 LR_4 LR_8;
+	node [shape = circle];
+	LR_0 -> LR_2 [ label = "SS(B)" ];
+	LR_0 -> LR_1 [ label = "SS(S)" ];
+	LR_1 -> LR_3 [ label = "S($end)" ];
+	LR_2 -> LR_6 [ label = "SS(b)" ];
+	LR_2 -> LR_5 [ label = "SS(a)" ];
+	LR_2 -> LR_4 [ label = "S(A)" ];
+	LR_5 -> LR_7 [ label = "S(b)" ];
+	LR_5 -> LR_5 [ label = "S(a)" ];
+	LR_6 -> LR_6 [ label = "S(b)" ];
+	LR_6 -> LR_5 [ label = "S(a)" ];
+	LR_7 -> LR_8 [ label = "S(b)" ];
+	LR_7 -> LR_5 [ label = "S(a)" ];
+	LR_8 -> LR_6 [ label = "S(b)" ];
+	LR_8 -> LR_5 [ label = "S(a)" ];
+}
+
   `)[0]
-      }
-    />
+          }
+          nodeRender={(n) => (
+            <text x={n.point.x} y={n.point.y}>
+              {n.data.id}
+            </text>
+          )}
+          edgeRender={(n) => (
+            <line
+              x1={n.points[0].x}
+              y1={n.points[0].y}
+              x2={n.points[n.points.length - 1].x}
+              y2={n.points[n.points.length - 1].y}
+              stroke="steelblue"
+            />
+          )}
+        />
+      </g>
+    </svg>
   );
 };
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,18 +1,27 @@
-import React, { memo, useMemo } from "react";
+import React, { Fragment, memo, useMemo } from "react";
 // @ts-expect-error
 import dagre from "@dagrejs/dagre";
 import { Graph, Node, Edge } from "./parser";
 
 export type Props = {
   data: Graph;
-  graphRender?: (node: Graph) => React.ReactNode;
-  nodeRender?: (node: Node) => React.ReactNode;
-  edgeRender?: (node: Edge) => React.ReactNode;
+  graphRender?: (node: { data: Graph }) => React.ReactNode;
+  nodeRender: (node: {
+    data: Node;
+    point: { x: number; y: number };
+  }) => React.ReactNode;
+  edgeRender: (node: {
+    data: Edge;
+    points: { x: number; y: number }[];
+  }) => React.ReactNode;
 };
 
 export const Renderer = memo(
   ({ data, graphRender, nodeRender, edgeRender }: Props) => {
-    const graph = useMemo(() => {
+    const [graph, nodeMap, edgeMap] = useMemo(() => {
+      const nodeMap: { [key: string]: Node } = {};
+      const edgeMap: { [key: string]: Edge } = {};
+
       const g = new dagre.graphlib.Graph({
         directed: data.type === "digraph",
         multigraph: true,
@@ -30,6 +39,7 @@ export const Renderer = memo(
           node.edges.forEach(processEdge);
         } else if (node.type === "node") {
           g.setNode(node.id, { width: 10, height: 10 });
+          nodeMap[node.id] = node;
           if (parentId) {
             g.setParent(node.id, parentId);
           }
@@ -37,6 +47,7 @@ export const Renderer = memo(
       };
       const processEdge = (edge: Edge) => {
         g.setEdge(edge.from, edge.to);
+        edgeMap[`${edge.from}-${edge.to}`] = edge;
       };
 
       data.nodes.forEach((node) => {
@@ -45,36 +56,27 @@ export const Renderer = memo(
       data.edges.forEach(processEdge);
 
       dagre.layout(g);
-      return g;
+      return [g, nodeMap, edgeMap];
     }, [data]);
 
     const nodes: React.ReactNode[] = [];
-    graph.nodes().forEach((v: any) => {
-      const n = graph.node(v);
+    graph.nodes().forEach((id: any) => {
+      const n = graph.node(id);
       nodes.push(
-        <text key={v} x={n.x} y={n.y}>
-          {v}
-        </text>
+        <Fragment key={id}>
+          {nodeRender({ data: nodeMap[id], point: { x: n.x, y: n.y } })}
+        </Fragment>
       );
     });
-    graph.edges().forEach((v: any) => {
-      const points = graph.edge(v).points;
+    graph.edges().forEach((e: any) => {
+      const points = graph.edge(e).points;
       nodes.push(
-        <line
-          key={`${v.v}-${v.w}`}
-          x1={points[0].x}
-          y1={points[0].y}
-          x2={points[points.length - 1].x}
-          y2={points[points.length - 1].y}
-          stroke="steelblue"
-        />
+        <Fragment key={`${e.v}-${e.w}`}>
+          {edgeRender({ data: edgeMap[`${e.v}-${e.w}`], points })}
+        </Fragment>
       );
     });
 
-    return (
-      <svg width="1000" height="1000">
-        <g transform="translate(50,50)">{nodes}</g>
-      </svg>
-    );
+    return <>{nodes}</>;
   }
 );
