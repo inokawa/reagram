f7b70e570f6ea1ca6ca4b312aca94d33fd2f7c35
inokawa <48897392+inokawa@users.noreply.github.com>
2021-01-15 22:10:37 +0900

    Add story

--- examples/index.stories.tsx
+++ examples/index.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import cluster from "../fixtures/cluster.gv";
 import finite_automaton from "../fixtures/finite_automaton.gv";
+import unix_family_tree from "../fixtures/unix_family_tree.gv";
 
 import Dot, { parse } from "../src";
 
@@ -40,6 +41,32 @@ export const FiniteStateMachine = () => {
       <g transform="translate(50,50)">
         <Dot
           data={parse(finite_automaton)[0]}
+          nodeRender={(n) => (
+            <text x={n.point.x} y={n.point.y}>
+              {n.data.id}
+            </text>
+          )}
+          edgeRender={(n) => (
+            <line
+              x1={n.points[0].x}
+              y1={n.points[0].y}
+              x2={n.points[n.points.length - 1].x}
+              y2={n.points[n.points.length - 1].y}
+              stroke="steelblue"
+            />
+          )}
+        />
+      </g>
+    </svg>
+  );
+};
+
+export const UnixFamilyTree = () => {
+  return (
+    <svg width="1000" height="1000">
+      <g transform="translate(50,50)">
+        <Dot
+          data={parse(unix_family_tree)[0]}
           nodeRender={(n) => (
             <text x={n.point.x} y={n.point.y}>
               {n.data.id}
